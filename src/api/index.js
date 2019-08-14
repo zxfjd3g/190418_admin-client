6c540299cf82cf88e0fe0cfac9297b047ec91c65
zxfjd3g <258147149@qq.com>
2019-08-14 14:49:21 +0800

    jsonp

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,8 @@
 包含n个接口请求函数的模块, 每个函数的返回值都是promise对象
 根据接口文档编写
 */
+import jsonp from 'jsonp'
+import { message } from 'antd'
 import ajax from './ajax'
 
 // const BASE = 'http://localhost:5000'
@@ -24,3 +26,24 @@ export const reqAddUser = (user) => ajax({
   method: 'POST',
   data: user
 })
+
+
+/* 
+获取天气信息(jsonp) 
+*/
+export const reqWeather = (city) => {
+  const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+  
+  return new Promise((resolve, reject) => { // 执行器函数
+    jsonp(url, {}, (err, data) => {
+      if (!err && data.error === 0) {
+        const {dayPictureUrl, weather} = data.results[0].weather_data[0]
+        resolve({dayPictureUrl, weather})
+      } else {
+        // reject(err)
+        message.error('获取天气信息失败!')
+      }
+    })
+  })
+  
+}
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,16 +1,94 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+
+import memoryUtils from '../../utils/memoryUtils'
+import menuConfig from '../../config/menuConfig'
+import { formateDate } from '../../utils/dateUtils'
+import {reqWeather} from '../../api'
 
 import './index.less'
 
 /* 
 Admin的左侧导航组件
 */
-export default class Header extends Component {
+class Header extends Component {
+
+  state = {
+    currentTime: formateDate(Date.now()),
+    dayPictureUrl: '', 
+    weather: ''
+  }
+
+  updateTime = () => {
+    // 启动循环定时器, 每隔1s更新一下时间状态
+    setInterval(() => {
+      const currentTime = formateDate(Date.now())
+      this.setState({
+        currentTime
+      })
+    }, 1000);
+  }
+
+  getWeather = async () => {
+    const { dayPictureUrl, weather } = await reqWeather('北京')
+    this.setState({
+      dayPictureUrl,
+      weather
+    })
+  }
+
+  /* 
+  异步获取天气信息显示
+  */
+
+  componentDidMount () {
+    this.updateTime()
+    this.getWeather()
+  }
+
+  /* 
+  得到当前请求对应的标题
+  */
+  getTitle = () => {
+    let title = ''
+    const path = this.props.location.pathname
+    menuConfig.forEach(item => {
+      if (item.key===path) {
+        title = item.title
+      } else if (item.children) {
+        const cItem = item.children.find(cItem => cItem.key===path)
+        if (cItem) {
+          title = cItem.title
+        }
+      }
+    })
+
+    return title
+  }
+
   render() {
+    const { currentTime, dayPictureUrl, weather } = this.state
+    const user = memoryUtils.user
+    // 得到当前请求对应的标题
+    const title = this.getTitle()
+
     return (
       <div className="header">
-        Header
+        <div className="header-top">
+          <span>欢迎, {user.username} </span>
+          <a href="javascript:">退出</a>
+        </div>
+        <div className="header-bottom">
+          <div className="header-bottom-left">{ title }</div>
+          <div className="header-bottom-right">
+            <span>{ currentTime }</span>
+            {dayPictureUrl ? <img src={dayPictureUrl} alt="weather" /> : null}
+            <span>{weather}</span>
+          </div>
+        </div>
       </div>
     )
   }
 }
+
+export default withRouter(Header)
